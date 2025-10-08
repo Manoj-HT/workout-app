877618d1bddff997b993b7bf61ae845f4879ca71
Manoj-HT <manulogan101@gmail.com>
2025-10-08 08:54:41 +0530

    # 6

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.magibuilds.workoutapp',
   appName: 'workout-app',
-  webDir: 'dist'
+  webDir: 'dist/workout-app/browser'
 };
 
 export default config;
